4f0904aebdc75526c6b1103ff430695b22de1caf
Ashutosh Anand Tiwari <ashutosh.tiwari@codemonk.in>
2022-06-25 23:01:18 +0530

    nuxt-link

--- pages/User/index.vue
+++ pages/User/index.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
     <h1>New User Page</h1>
+
+    <input type="number" v-model="userId" />
+    <button @click="onLoadUser">Load User</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      userId: "",
+    };
+  },
+
+  methods: {
+    onLoadUser() {
+      this.$router.push("/user/" + this.userId);
+    },
+  },
+};
 </script>
 
 <style scoped>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <div class="">
+    <!-- <Tutorial/> -->
+
+    <!-- <a href="/user">Go to User ( tal to server no prevent default)</a>
+
+    <router-link to="/user">Go to user (using router link)</router-link> -->
+    <nuxt-link to="/user">Go to user (using nuxt link)</nuxt-link>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
